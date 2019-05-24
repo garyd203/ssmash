9506b474b4778be31c007e176cdfe0d8626f72b1
Gary Donovan <gazza@gazza.id.au>
2019-05-24 11:23:13 +1000

    #6: Wire up the CLI to the converter

--- src/ssmash/cli.py
+++ src/ssmash/cli.py
@@ -9,13 +9,20 @@ from datetime import datetime, timezone
 import yaml
 from flyingcircus.core import Stack
 
+from ssmash.converter import convert_hierarchy_to_ssm
+
 
 @click.command(help=__doc__)
 @click.argument("input", type=click.File("r"), default="-")
 @click.argument("output", type=click.File("w"), default="-")
 def create_stack(input, output):
     # Load source config
-    config = yaml.safe_load(input)
+    #
+    # Note that PyYAML returns None for an empty file, rather than an empty
+    # dictionary
+    appconfig = yaml.safe_load(input)
+    if appconfig is None:
+        appconfig = {}
 
     # Create stack
     stack = Stack(Description="")  # FIXME desc
@@ -27,8 +34,8 @@ def create_stack(input, output):
         "version": __version__,
     }
 
-    # FIXME Traverse config hierarchy
-    # create_params_for_dict(stack, config, "/")
+    # Create a parameter for each element in the config hierarchy.
+    convert_hierarchy_to_ssm(appconfig, stack)
 
     # Write YAML to the specified file
     output.write(stack.export("yaml"))
--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,4 +1,4 @@
-"""Tests for command line interface."""
+"""Tests for the command line interface."""
 
 from datetime import datetime, timezone
 
@@ -10,81 +10,83 @@ from freezegun import freeze_time
 from ssmash import cli
 
 SIMPLE_INPUT = """foo: bar"""
-SIMPLE_OUTPUT_LINE = "some example output line"  # FIXME
+SIMPLE_OUTPUT_LINE = "Name: /foo"
 
 
-def test_should_display_help():
-    runner = CliRunner()
-    help_result = runner.invoke(cli.create_stack, ["--help"])
-    assert help_result.exit_code == 0
-    assert "--help  Show this message and exit." in help_result.output
+class TestBasicCLI:
+    def test_should_display_help(self):
+        runner = CliRunner()
+        help_result = runner.invoke(cli.create_stack, ["--help"])
+        assert help_result.exit_code == 0
+        assert "--help  Show this message and exit." in help_result.output
+
+    def test_should_exit_cleanly_with_empty_input(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack)
+        assert result.exit_code == 0
+        assert not result.stderr_bytes
 
 
-def test_should_exit_cleanly_with_empty_input():
-    runner = CliRunner()
-    result = runner.invoke(cli.create_stack)
-    assert result.exit_code == 0
-    assert not result.stderr_bytes
-
-
-def test_should_convert_simple_input_with_default_pipes():
-    # Exercise
-    runner = CliRunner()
-    result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
-
-    # Verify
-    assert result.exit_code == 0
-    assert not result.stderr_bytes
-
-    assert SIMPLE_OUTPUT_LINE in result.stdout
-
-
-def test_should_convert_simple_input_with_files():
-    input_filename = "input.yaml"
-    output_filename = "output.yaml"
-
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        with open(input_filename, "w") as f:
-            f.write(SIMPLE_INPUT)
+class TestCloudFormationMetadata:
+    def test_output_should_contain_version(self):
+        from ssmash import __version__ as package_version
 
         # Exercise
-        result = runner.invoke(cli.create_stack, args=[input_filename, output_filename])
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+
+        # Verify
+        cfn = yaml.safe_load(result.stdout)
+        ssmash_version = cfn["Metadata"]["ssmash"]["version"]
+        assert ssmash_version == package_version
+        semver.parse(ssmash_version)
+
+    def test_output_should_contain_timestamp(self):
+        expected_time = datetime(2019, 5, 22, 1, 2, 3, tzinfo=timezone.utc)
+
+        # Exercise
+        runner = CliRunner()
+
+        with freeze_time(expected_time):
+            result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+
+        # Verify
+        cfn = yaml.safe_load(result.stdout)
+        timestamp = cfn["Metadata"]["ssmash"]["generated_timestamp"]
+        assert timestamp == "2019-05-22T01:02:03+00:00"
+
+
+class TestCloudFormationIsProduced:
+    def test_should_convert_simple_input_with_default_pipes(self):
+        # Exercise
+        runner = CliRunner()
+        result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
 
         # Verify
         assert result.exit_code == 0
-        assert not result.stdout
         assert not result.stderr_bytes
 
-        with open(output_filename, "r") as f:
-            actual_output = f.read()
-        assert SIMPLE_OUTPUT_LINE in actual_output
+        assert SIMPLE_OUTPUT_LINE in result.stdout
 
+    def test_should_convert_simple_input_with_files(self):
+        input_filename = "input.yaml"
+        output_filename = "output.yaml"
 
-def test_output_should_contain_version():
-    from ssmash import __version__ as package_version
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            with open(input_filename, "w") as f:
+                f.write(SIMPLE_INPUT)
 
-    # Exercise
-    runner = CliRunner()
-    result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
+            # Exercise
+            result = runner.invoke(
+                cli.create_stack, args=[input_filename, output_filename]
+            )
 
-    # Verify
-    cfn = yaml.safe_load(result.stdout)
-    ssmash_version = cfn["Metadata"]["ssmash"]["version"]
-    assert ssmash_version == package_version
-    semver.parse(ssmash_version)
+            # Verify
+            assert result.exit_code == 0
+            assert not result.stdout
+            assert not result.stderr_bytes
 
-
-def test_output_should_contain_timestamp():
-    expected_time = datetime(2019, 5, 22, 1, 2, 3, tzinfo=timezone.utc)
-
-    # Exercise
-    runner = CliRunner()
-
-    with freeze_time(expected_time):
-        result = runner.invoke(cli.create_stack, input=SIMPLE_INPUT)
-
-    # Verify
-    cfn = yaml.safe_load(result.stdout)
-    timestamp = cfn["Metadata"]["ssmash"]["generated_timestamp"]
-    assert timestamp == "2019-05-22T01:02:03+00:00"
+            with open(output_filename, "r") as f:
+                actual_output = f.read()
+            assert SIMPLE_OUTPUT_LINE in actual_output
