32f2a6a1691a5a78327981ff8c60101ce46082d8
Gary Donovan <gazza@gazza.id.au>
2019-06-05 20:22:24 +1000

    Start v1.1.1-beta

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/garyd203/ssmash",
-    version="1.1.0",
+    version="1.1.1-beta",
     zip_safe=False,
 )
